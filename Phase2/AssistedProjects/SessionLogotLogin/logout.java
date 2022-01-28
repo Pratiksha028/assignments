4f98672052d7df417458b85bbab771ea797ec0c3
Pratiksha028 <97235528+Pratiksha028@users.noreply.github.com>
2022-01-28 21:13:35 +0530

    Create logout.java

---
+++ Phase2/AssistedProjects/SessionLogotLogin/logout.java
@@ -0,0 +1,37 @@
+package javatraining;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Logout() {
+        super();
+        
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session=request.getSession();  
+        session.invalidate();           
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+    out.println("Logged out of session.<br>");
+    out.println("</body></html>");
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
