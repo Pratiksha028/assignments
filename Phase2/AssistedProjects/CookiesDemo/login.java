a785b2c71504692abc2363a6a87bdfccdc5ef125
Pratiksha028 <97235528+Pratiksha028@users.noreply.github.com>
2022-01-28 21:18:39 +0530

    Create login.java

---
+++ Phase2/AssistedProjects/CookiesDemo/login.java
@@ -0,0 +1,32 @@
+package javatraining;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public Login() {
+        super();
+        
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Cookie userId = new Cookie("userid", request.getParameter("userid"));
+        response.addCookie(userId);
+     response.sendRedirect("dashboard");  
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
