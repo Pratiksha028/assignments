a66379bc04c2063d46e5fbcb66c16635c1a33469
Pratiksha028 <97235528+Pratiksha028@users.noreply.github.com>
2022-01-28 21:20:45 +0530

    Create dashboard.java

---
+++ Phase2/AssistedProjects/CookiesURL/dashboard.java
@@ -0,0 +1,40 @@
+package javatraining;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/dashboard")
+public class Dashboard extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public Dashboard() {
+        super();
+       
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        
+        String userId = request.getParameter("userid");
+        if (userId == null ) {
+            out.println("No UserId was found in the URL.<br>");
+        } else {
+            out.println("UserId obtained from URL Rewriting:" + userId + "<br>");
+        }
+        out.println("</body></html>");
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
