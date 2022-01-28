e9558d474145fe50721acce56513c3d3587f8d4c
Pratiksha028 <97235528+Pratiksha028@users.noreply.github.com>
2022-01-28 20:29:30 +0530

    Create post.java

---
+++ Phase2/AssistedProjects/GetPost/post.java
@@ -0,0 +1,41 @@
+package javatraining;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/post")
+public class PostProgram extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public PostProgram() {
+        super();
+        
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        
+        String name = request.getParameter("name");
+        String address = request.getParameter("address");
+        
+        out.println("Name=" + name + "<br>Address=" + address);
+        out.println("</body></html>");
+
+	}
+
+}
