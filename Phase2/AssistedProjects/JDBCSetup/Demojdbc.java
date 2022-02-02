454d4a9384ce57892e3bd3c2abe293f9a17d5ff9
Pratiksha028 <97235528+Pratiksha028@users.noreply.github.com>
2022-02-02 20:53:05 +0530

    Create Demojdbc.java

---
+++ Phase2/AssistedProjects/JDBCSetup/Demojdbc.java
@@ -0,0 +1,56 @@
+package javatraining;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Demojdbc")
+public class Demojdbc extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public Demojdbc() {
+        super();
+       
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+            PrintWriter out = response.getWriter();
+            out.println("<html><body>");
+            
+           InputStream in = getServletContext().getResourceAsStream("/WEB-INF/config.properties");
+           Properties props = new Properties();
+           props.load(in);
+           
+           Dbconnection conn = new Dbconnection(props.getProperty("url"), props.getProperty("userid"), props.getProperty("password"));
+           out.println("DB Connection initialized.<br>");
+           
+           conn.closeConnection();
+           out.println("DB Connection closed.<br>");
+           
+           
+           out.println("</body></html>");
+           conn.closeConnection();
+           
+   } catch (ClassNotFoundException e) {
+           e.printStackTrace();
+   } catch (SQLException e) {
+           e.printStackTrace();
+   }
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+		doGet(request, response);
+	}
+
+}
