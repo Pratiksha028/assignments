c7d3f52ab5396886842b0824ba1f45fa3d5917f7
Pratiksha028 <97235528+Pratiksha028@users.noreply.github.com>
2022-02-14 21:21:07 +0530

    Create  UserManagerApplication.java

---
+++ Phase3/PracticeProjects/UserManager/UserManagerApplication.java
@@ -0,0 +1,10 @@
+ 
+	package com.training.UserManager;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class UserManagerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagerApplication.class, args);
+	}
+}
