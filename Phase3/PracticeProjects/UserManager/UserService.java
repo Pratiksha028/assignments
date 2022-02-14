bc0a9706755c205411063e7b0776f1107a8beadb
Pratiksha028 <97235528+Pratiksha028@users.noreply.github.com>
2022-02-14 21:59:54 +0530

    Create UserService.java

---
+++ Phase3/PracticeProjects/UserManager/UserService.java
@@ -0,0 +1,28 @@
+
+package com.training.UserManager.services;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.training.UserManager.entities.User;
+import com.training.UserManager.exceptions.UserNotFoundException;
+import com.training.UserManager.repositories.UserRepository;
+@Service 
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+	public Iterable<User> GetAllUsers() {
+	return userRepository.findAll();
+	}
+	public User GetUserByName(String name) {
+	return userRepository.findByName(name);
+	}
+	public User GetUserById(Integer id) {
+	Optional<User> foundUser = userRepository.findById(id);
+	if(!foundUser.isPresent()) throw new UserNotFoundException();
+	return foundUser.get();
+	}
+	public User UpdateUser(User userToUpdate) {
+	return userRepository.save(userToUpdate);
+	}
+}
+
