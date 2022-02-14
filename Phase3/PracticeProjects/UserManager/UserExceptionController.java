4def1f97c35bf11d24c59f3d21e180377dadda91
Pratiksha028 <97235528+Pratiksha028@users.noreply.github.com>
2022-02-14 21:57:15 +0530

    Create  UserExceptionController.java

---
+++ Phase3/PracticeProjects/UserManager/UserExceptionController.java
@@ -0,0 +1,15 @@
+           
+package com.training.UserManager.controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import com.training.UserManager.exceptions.UserNotFoundException;
+@ControllerAdvice
+public class UserExceptionController {
+	@ExceptionHandler(value=UserNotFoundException.class)
+	public ResponseEntity<Object> exception(UserNotFoundException ex) {
+		return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
+	}
+}
+
