b2c558dedf9e71b735447212414898ce0941bcad
Pratiksha028 <97235528+Pratiksha028@users.noreply.github.com>
2022-02-16 00:32:14 +0530

    Create CategoriesService.java

---
+++ Phase3/assessment/SportyShoes/CategoriesService.java
@@ -0,0 +1,32 @@
+
+
+package com.training.SportyShoes.service;
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.training.SportyShoes.entity.Categories;
+import com.training.SportyShoes.jdbc.CategoriesRepository;
+
+@Service
+@Transactional
+public class CategoriesService {
+	@Autowired
+	private CategoriesRepository catrepo;
+	public List<Categories> listAll()
+	{
+		return catrepo.findAll();
+	}
+	public void save(Categories categories)
+	{
+		catrepo.save(categories);
+	}
+		public Categories get(long id)
+	{
+		return catrepo.findById(id).get();
+	}
+	public void delete(long id)
+	{
+		catrepo.deleteById(id);
+	}
+}
