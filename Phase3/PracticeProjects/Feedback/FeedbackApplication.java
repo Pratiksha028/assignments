2c6e6aca846c97560b34caf0289abee281544eca
Pratiksha028 <97235528+Pratiksha028@users.noreply.github.com>
2022-02-15 23:48:52 +0530

    Create FeedbackApplication.java

---
+++ Phase3/PracticeProjects/Feedback/FeedbackApplication.java
@@ -0,0 +1,9 @@
+
+importorg.springframework.boot.SpringApplication;
+importorg.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class FeedbackApplication {
+public static void main(String[] args) {
+SpringApplication.run(FeedbackApplication.class, args);
+}
+}
