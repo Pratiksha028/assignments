9233955561072bd0fd283d86ad644a3d32e7ede7
Pratiksha028 <97235528+Pratiksha028@users.noreply.github.com>
2022-03-10 23:09:42 +0530

    Create MyApplication.java

---
+++ Phase5/Assessment/CICD Deployment/MyApplication.java
@@ -0,0 +1,14 @@
+
+
+package com.simplilearn.workshop;
+import org.springframework.boot.SpringApplication;
+importorg.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplication.class, args);
+	}
+}
+
+
