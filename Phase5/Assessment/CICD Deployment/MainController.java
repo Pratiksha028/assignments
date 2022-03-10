6371d069620da54ba01bf51c53ba8a241e85b75c
Pratiksha028 <97235528+Pratiksha028@users.noreply.github.com>
2022-03-10 23:10:10 +0530

    Create MainController.java

---
+++ Phase5/Assessment/CICD Deployment/MainController.java
@@ -0,0 +1,15 @@
+
+package com.simplilearn.workshop.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+publicclass MainController {
+
+@RequestMapping("/")
+public String index() {
+return"index.html";
+  }
+}
+
