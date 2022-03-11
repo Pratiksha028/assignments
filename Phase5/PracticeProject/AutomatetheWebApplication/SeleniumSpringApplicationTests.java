6a9d714b269daf0bb09137a399cb8921c67abe6c
Pratiksha028 <97235528+Pratiksha028@users.noreply.github.com>
2022-03-11 15:30:08 +0530

    Create SeleniumSpringApplicationTests.java

---
+++ Phase5/PracticeProject/AutomatetheWebApplication/SeleniumSpringApplicationTests.java
@@ -0,0 +1,15 @@
+
+
+package com.demo.seleniumspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
